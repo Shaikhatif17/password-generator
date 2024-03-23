300f9d637f82edeb67341bbfe32df5a15760d91e
atif shaikh <shaikhatif456a@gmail.com>
2024-03-23 14:53:30 +0530

    add managar component

---
+++ src/components/Manager.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Manager() {
+  return (
+    <>
+      <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
+      
+      <div className='container mx-auto bg-slate-800'></div>
+    
+      <div className="text-white flex flex-col p-4">
+       <input type="text" />
+        <div className="flex">
+            <input type="text" />
+            <input type="text" />
+        </div>
+       </div>
+      </div>
+    </>
+  )
+}
+
+export default Manager
